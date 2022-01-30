605e6ed1fdf56e3a0cfb38a157949ab0e4761d2e
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-01-30 20:26:45 +0200

    Add routing to main server file.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
+const routeUsers = "users";
+import router, * as users from "./routes/userRoute";
+
 const port = 8080;
 const app = express();
 
@@ -11,9 +14,7 @@ app.use(express.json());
 /**
  * The server of the project.
  */
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(`/${routeUsers}`, router);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
