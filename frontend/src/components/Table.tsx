4e1ae6e4d0b33df1227327443aeb1c3bb344c96c
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-02-02 18:52:37 +0200

    Create main functionality to fetch list of items.

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface IInfo {
+  id: number;
+  fName: string;
+  lName: string;
+  age: number;
+}
+const defaultList: IInfo[] = [];
 
 function Table() {
-  return <p>Yes</p>;
+  const [list, setList]: [IInfo[], (lists: IInfo[]) => void] =
+    useState(defaultList);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await axios.get<IInfo[]>(`http://localhost:8080/users`);
+      setList(response.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <ul>
+        {list.map((id) => (
+          <li>
+            {id.id} {id.fName} {id.lName} {id.age}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Table;
