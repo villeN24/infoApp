b98c4e6fc2d55daacc4e46944a4ccf7c4f253653
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-02-02 16:31:10 +0200

    Remove autogenerated code and change it to return TableComponent.

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import TableComponent from "./components/TableComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <TableComponent />
+    </div>
+  );
+}
+
+export default App;
